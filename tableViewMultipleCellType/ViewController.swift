730ad0b4a8feb8f08650e42eaa32d7630e48524e
trinhquocmanh1990 <trinhquocmanh1990@gmail.com>
2018-12-14 09:48:04 +0700

    Initial Commit

---
+++ tableViewMultipleCellType/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableViewMultipleCellType
+//
+//  Created by trinhquocmanh-mac 10.12 on 12/14/18.
+//  Copyright © 2018 trinhquocmanh-mac 10.12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
